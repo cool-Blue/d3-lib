2fa88c5884fab8e3a1808856dabcdec99bc12b1d
cool-Blue <cool.blue@y7mail.com>
2015-08-26 00:15:13 +1000

    end-all  - added

---
+++ transitions/end-all/endAll.js
@@ -0,0 +1,7 @@
+function endall(transition, callback) {
+	var n = 0;
+	transition
+		.each(function() { ++n; })
+		.each("end", function() { if (!--n) callback.apply(this, arguments); });
+	if(transition.empty()) callback();
+}
