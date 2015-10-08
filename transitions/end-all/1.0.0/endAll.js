6890ba9f17f700732482ef77b5c3152094ca4559
cool-Blue <cool.blue@y7mail.com>
2015-10-08 18:59:08 +1100

    transitions  endAll 1.0.0  - added namespacing to group different transitions to the same callback

--- transitions/end-all/1.0.0/endAll.js
+++ transitions/end-all/1.0.0/endAll.js
@@ -1,8 +1,31 @@
-if(!d3.cbTransition) d3.cbTransition = {};
-d3.cbTransition.endAll = function endAll(transition, callback) {
-	var n = 0;
-	transition
-		.each(function() { ++n; })
-		.each("end.endAll", function() { if (!--n) callback.apply(this, arguments); });
-	if(transition.empty()) callback();
-}
+(function(d3) {
+    if(!d3.cbTransition) d3.cbTransition = {};
+    var groups = d3.map();
+    d3.cbTransition.endAll = function endAll(transition, callback, group) {
+        // invoke the last registerd callback after all of the members of the
+        // transitions registered under the group namespace have completed.
+        // e.g.
+        //  trans1.call(d3.cbTransition.endAll, callback1, "myGroup")
+        //  trans2.call(d3.cbTransition.endAll, callback2, "myGroup")
+        // will invoke callback2 after all members of trans1 and trans2 are complete
+        // (callback1 is ignored)
+
+        // register the callback with ns as key (which is null if no group)
+        var members;
+        groups.set(group, (members = groups.get(group)) ? (members.push(callback), members) : [callback]);
+
+        function callbackNS(context, args, namespace){
+            var g, cb = (g = groups.get(namespace)).pop();
+            if (!g.length) {
+                cb.apply(context, args)
+                groups.remove(namespace)
+            }
+        }
+
+        var n = 0;
+        transition
+            .each(function() { ++n; })
+            .each("end.endAll", function() { if(!--n) callbackNS(this, arguments, group); });
+        if(transition.empty()) callback();
+    }
+})(d3);
