556ca030e58a0b7acc1e40e6607ee5db2c8db5ef
cool-Blue <cool.blue@y7mail.com>
2015-10-08 23:18:15 +1100

    inputs  select   select.js   - bind arbitrary events

--- inputs/select/select.js
+++ inputs/select/select.js
@@ -17,11 +17,12 @@
 		// if the text and value is the same then data is scalar array
 		// 	otherwise the data elements must have text and value fields
 		//
-		var select = (config.base ? (config.base.append ? config.base : d3.select(config.base))
-															: d3.select("body"))
-					[config.before ? "insert": "append"]("select", config.before ? config.before : null)
-					.on(config.on || "input", config.onUpdate)  // bind the listener to the outer element
-					.data([config.data]),
+		var select = (config.base ?
+                      (config.base.append ? config.base : d3.select(config.base)) :
+                      d3.select("body"))
+                [config.before ? "insert": "append"]("select", config.before ? config.before : null)
+                .each(hookEvents)
+                .data([config.data]),
 				options = select.selectAll("option").data(function(d){return d});
 		options.enter().append("option");
 		options.exit().remove();
@@ -37,6 +38,32 @@
 				return this[0].parentNode.value
 			}
 		});
+        function hookEvents() {
+            // store the DOM element
+            var _control = this;
+            // the bound data is the config object for the control
+            // parse the keys and bind a listener to any key beginning with "on"
+            Object.keys(config).filter(function(k) {
+                return k.slice(0, 2) == "on";
+            }).map(function(p, i, listeners) {
+                // strip the event name off the listener
+                var e = p.slice(2);
+
+                if(!config.on)
+                // lazily create a dispatch with the events found in config
+                    config.on = d3.dispatch
+                        .apply(null, listeners.map(function(l) {
+                            return l.slice(2);
+                        }));
+
+                config.on.on(e, config[p]);
+                // then hook the dispatch to the element event
+                d3.select(_control).on(e, function() {
+                    config.on[e].apply(this, arguments)
+                })
+
+            });
+        }
 	}
 	//experiment with chaining...
 	d3.ui.select2 = function (base){
