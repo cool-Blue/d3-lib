ef2d3e6ef386dfbd9666da0cfa9963545815064d
cool-Blue <cool.blue@y7mail.com>
2015-10-08 16:50:52 +1100

    transitions - namespaced end event listener

--- transitions/end-all/endAll.js
+++ transitions/end-all/endAll.js
@@ -2,6 +2,6 @@ function endAll(transition, callback) {
 	var n = 0;
 	transition
 		.each(function() { ++n; })
-		.each("end", function() { if (!--n) callback.apply(this, arguments); });
+		.each("end.endAll", function() { if (!--n) callback.apply(this, arguments); });
 	if(transition.empty()) callback();
 }
